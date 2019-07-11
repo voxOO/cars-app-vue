6ca2c8da20eca984dc61a361798ce93fc90de595
vox <vojislav.stupar@gmail.com>
2019-07-11 14:44:10 +0200

    zad5 alert preview

--- src/components/AddCar.vue
+++ src/components/AddCar.vue
@@ -17,14 +17,14 @@
         </div>
         <div class="form-group">
             <label for='maxSpeed'>Max Speed :</label>
-            <input type="number" id="maxSpeed" class="form-control" v-model="newCar.maxSpeed" required>
+            <input type="number" id="maxSpeed" class="form-control" v-model.number="newCar.maxSpeed" required>
         </div>
         <div class="form-group">
             <label for='numberOfDoors'>Number Of Doors :</label>
-            <input type="number" id="numberOfDoors" class="form-control" v-model="newCar.numberOfDoors" required>
+            <input type="number" id="numberOfDoors" class="form-control" v-model.number="newCar.numberOfDoors" required>
         </div>
         <div class="form-group">
-            <input type="radio" name="gearBox"  value="isAutomatic" v-model="newCar.isAutomatic"> isAutomatic 
+            <input type="checkbox" name="isAutomatic"  value=true v-model="newCar.isAutomatic"> isAutomatic 
             <input type="radio" name="engine1" value='diesel' v-model="newCar.engine"> Diesel
             <input type="radio" name="engine2" value='petrol' v-model="newCar.engine"> Petrol
             <input type="radio" name="engine3" value='electric' v-model="newCar.engine"> Electric
@@ -35,6 +35,7 @@
             <button type="submit" class="btn btn-primary" >Add Car</button>
         </div>
          <p class="btn btn-secondary" @click="resetForm">Reset</p>
+         <p class="btn btn-secondary" @click="previewInput">Preview</p>
    </form>
     </div>
 </template>
@@ -49,11 +50,11 @@ export default {
             newCar: {
                 "brand": "",
                 "model": "",
-                "year": '',
-                "maxSpeed": '',
-                "isAutomatic": '',
+                "year": 0,
+                "maxSpeed": 0,
+                "numberOfDoors": 0,
+                "isAutomatic": false,
                 "engine": "",
-                "numberOfDoors": '',
             }
         }
     },
@@ -72,17 +73,35 @@ export default {
        })
     },
     resetForm() {
-      this.newCar.brand= 
-      this.newCar.model= 
-      this.newCar.maxSpeed =
-      this.newCar.isAutomatic=
-      this.newCar.engine=
-      this.newCar.numberOfDoors= '';
-      
+      this.newCar.brand= ''
+      this.newCar.model= ''
+      this.newCar.year = 0
+      this.newCar.maxSpeed = 0
+      this.newCar.numberOfDoors= 0;
+      this.newCar.isAutomatic= false
+      this.newCar.engine= ''
+  },
+  previewInput() {
+      alert(
+          "Brand:" + this.newCar.brand +
+          "\nModel:" + this.newCar.model +
+          "\nYear:" + this.newCar.year +
+          "\nMax Speed:" + this.newCar.maxSpeed +
+          "\nNumber of doors:" + this.newCar.numberOfDoors +
+          "\nAutomatic:" + this.newCar.isAutomatic +
+          "\nEngine type:" + this.newCar.engine
+      );
   }
 }
 }
 </script>
+
+<style scoped>
+    form p {
+        margin-right:3px;
+    }
+</style>
+
 
 // Prikazati formu gde se upisuju svi potrebni podaci o automobilu
 // brand i model se unose pomoću text input-a
